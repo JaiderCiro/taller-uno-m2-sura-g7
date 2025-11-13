9d01717858eab13e93114d5b90df4bf145db2f26
Jaider Rendón Ciro <jaider.rendon.313@gmail.com>
2025-11-13 18:43:33 -0500

    Se realiza ejercicio #4

--- script.js
+++ script.js
@@ -40,19 +40,20 @@ console.log(historial);
 historial.pop()
 console.log(historial);
 
+//Ejercicio #4
+
+let colaImpresion = ["doc1.pdf", "doc2.png", "doc3.jpg"];
+console.log(colaImpresion);
 
 
+colaImpresion.shift()
+console.log(colaImpresion);
 
+colaImpresion.push("doc4.txt")
+console.log(colaImpresion);
 
-
-
-
-
-
-
-
-
-
+colaImpresion.shift()
+console.log(colaImpresion);
 
 
 
