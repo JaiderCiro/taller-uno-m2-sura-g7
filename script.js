170c0c31413a43ad94343628cfcfed5df2ff6d66
Jaider Rendón Ciro <jaider.rendon.313@gmail.com>
2025-11-13 15:46:53 -0500

    Se realiza ejercicio #1

---
+++ script.js
@@ -0,0 +1,22 @@
+//Ejercicio #1
+
+let compras = ["manzana"]
+console.log(compras);
+
+
+compras.push("leche", "pan");
+console.log (compras); 
+
+compras.unshift ("huevos");
+console.log(compras);
+
+compras.splice(1,1);
+console.log(compras);
+
+
+
+
+
+
+
+
