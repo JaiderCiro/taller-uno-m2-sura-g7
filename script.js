d3def0236632924e8d6b0d1633bdf50bda448fab
Jaider Rendón Ciro <jaider.rendon.313@gmail.com>
2025-11-16 15:08:04 -0500

    Se realiza ejercicio #13 (Receta de cocina)

--- script.js
+++ script.js
@@ -139,6 +139,20 @@ commits.push("v1.2-corregido");
 console.log(commits);
 
 
+//Ejercicio #13
+
+let receta = ["Paso 2: Mezclar ingredientes", "Paso 3: Hornear"];
+console.log(receta);
+
+receta.unshift("Paso 1: Pre-calentar horno");
+console.log(receta);
+
+receta.push("Paso 4: Servir");
+console.log(receta);
+
+
+
+
 
 
 
