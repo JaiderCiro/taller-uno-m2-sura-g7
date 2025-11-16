f24e965d18b98a89a6362dc05654916a4be98b64
Jaider Rendón Ciro <jaider.rendon.313@gmail.com>
2025-11-16 14:48:11 -0500

    Se realiza ejercicio #10 (Fill)

--- script.js
+++ script.js
@@ -102,6 +102,18 @@ estudiantes.sort()
 console.log(estudiantes);
 
 
+//Ejercicio #10
+
+let puntuaciones = [100, 85, 92, 78];
+console.log(puntuaciones);
+
+puntuaciones.fill(0);
+console.log(puntuaciones);
+
+
+
+
+
 
 
 
