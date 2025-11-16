2f532fca9f017063a3db2040a02d9ec98cc48edf
Jaider Rendón Ciro <jaider.rendon.313@gmail.com>
2025-11-16 14:59:25 -0500

    Se realiza ejercicio #12 (Simple)

--- script.js
+++ script.js
@@ -123,3 +123,24 @@ console.log(carrito);
 
 carrito.splice(1,1); 
 console.log(carrito);
+
+//Ejercicio #12 
+
+let commits = ["v1.0", "v1.1"];
+console.log(commits);
+
+commits.push("v1.2");
+console.log(commits);
+
+commits.pop("v1.2");
+console.log(commits);
+
+commits.push("v1.2-corregido"); 
+console.log(commits);
+
+
+
+
+
+
+
